d297fa1ec42c7406d9950278250ae828448d2a4e
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2023-10-15 00:34:59 +0800

    Add files via upload

---
+++ static/js/277.09fee849.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshop=self.webpackChunkshop||[]).push([[277],{1277:function(e,s,t){t.r(s);var h=t(184);s.default=function(){return(0,h.jsx)("img",{src:"/img/wrongpage.jpeg",style:{width:"80%",height:"80%"},alt:"404page"})}}}]);
+//# sourceMappingURL=277.09fee849.chunk.js.map
